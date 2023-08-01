9cb692271293d5be187ec68d0b588f973b4461dc
FikriPrtma <128881320+FikriPrtma@users.noreply.github.com>
2023-08-01 18:48:20 +0700

    Sedikit Penambahan Waktu pada Beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts/main')
-
+<link rel="stylesheet" href="public\assets\css\beranda\style.css">
 @section('body')
     <h1>Selamat datang, {{ auth()->user()->nama }}</h1>
+    <h1 id="current-time">12:00:00</h1>
+    <script>
+
+        let time = document.getElementById("current-time");
+
+        setInterval(() => {
+            let d = new Date();
+            time.innerHTML= d.toLocaleTimeString();
+        }, 1000)
+        
+
+    </script>
 @endsection
